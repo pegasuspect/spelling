6b8a17d4bc4bc2efe8c83be79d5a5ff539fa9408
Osman <1279406+pegasuspect@users.noreply.github.com>
2019-05-13 12:54:53 -0400

    initial

---
+++ index.js
@@ -0,0 +1,54 @@
+const readline = require('readline');
+const colors = require('colors');
+
+let dict = {
+	'a': 'Alice',
+	'b': 'Boston',
+	'c': 'Chicago',
+	'd': 'David',
+	'e': 'Edward',
+	'f': 'Frank',
+	'g': 'George',
+	'h': 'Hotel',
+	'i': 'India',
+	'j': 'James',
+	'k': 'Kate',
+	'l': 'Love',
+	'm': 'Mary',
+	'n': 'New York',
+	'o': 'Ocean',
+	'p': 'Peter',
+	'q': 'Queen',
+	'r': 'Romeo',
+	's': 'Samuel',
+	't': 'Tom',
+	'u': 'Uncle',
+	'v': 'Victor',
+	'w': 'William',
+	'x': 'X-ray',
+	'y': 'Young',
+	'z': 'Zebra',
+}
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(line){
+	let lines = line.split(' ')
+	lines.forEach(line => {
+		console.log(line.split('').
+			map(c => c.toLowerCase()).
+			map(char => dict[char] ? `${char.toUpperCase().underline.red} as in ${dict[char].inverse}` : `${char}`).
+			join('\t')
+		);
+	})
+ 
+	// console.log('hello'.green); // outputs green text
+	// console.log('i like cake and pies'.underline.red) // outputs red underlined text
+	// console.log('inverse the color'.inverse); // inverses the color
+	// console.log('OMG Rainbows!'.rainbow); // rainbow
+	// console.log('Run the trap'.trap); // Drops the bass
+})
